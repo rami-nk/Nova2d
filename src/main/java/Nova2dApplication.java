14376c913dc39417b0a856b5ae274c9b310a59ba
Rami Nakkar <rami.nakkar@miragon.io>
2023-02-16 21:12:06 +0100

    feat: add init window

---
+++ src/main/java/Nova2dApplication.java
@@ -0,0 +1,9 @@
+import io.nova.Nova2dWindow;
+
+public class Nova2dApplication {
+
+    public static void main(String[] args) {
+        var window = Nova2dWindow.get();
+        window.run();
+    }
+}
---
+++ src/main/java/io/nova/Nova2dWindow.java
@@ -0,0 +1,118 @@
+package io.nova;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import java.awt.*;
+import java.util.Objects;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+@Setter
+@Getter
+@RequiredArgsConstructor
+public class Nova2dWindow {
+
+    private static Nova2dWindow nova2dWindow;
+    private Integer width;
+    private Integer height;
+    @NonNull
+    private String title;
+    private Long glfwWindow;
+
+    public static Nova2dWindow get() {
+        if (Objects.isNull(nova2dWindow)) {
+            nova2dWindow = new Nova2dWindow("Nova2d");
+            var dimension = Toolkit.getDefaultToolkit().getScreenSize();
+            nova2dWindow.setHeight(dimension.height);
+            nova2dWindow.setWidth(dimension.width);
+            return nova2dWindow;
+        }
+        return nova2dWindow;
+    }
+
+    private static void printVersion() {
+        System.out.printf("Hello LWJGL %s !%n", Version.getVersion());
+    }
+
+    private static void terminateGLFWAndFreeErrorCallback() {
+        glfwTerminate();
+        Objects.requireNonNull(glfwSetErrorCallback(null)).free();
+    }
+
+    public void run() {
+        printVersion();
+
+        init();
+        loop();
+
+        freeWindowCallbacksAndDestroyWindow();
+        terminateGLFWAndFreeErrorCallback();
+    }
+
+    private void init() {
+        // Set up an error callback. The default implementation
+        // will print the error message in System.err.
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        if (!glfwInit())
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        // Configure GLFW
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE); // window in maximized position
+
+        // Create the window
+        glfwWindow = glfwCreateWindow(width, height, title, NULL, NULL);
+        if (glfwWindow == NULL)
+            throw new RuntimeException("Failed to create the GLFW window");
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(glfwWindow);
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        // Make the window visible
+        glfwShowWindow(glfwWindow);
+
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the GLCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GL.createCapabilities();
+    }
+
+    private void loop() {
+        // Set the clear color
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+
+        // Run the rendering loop until the user has attempted to close
+        // the window or has pressed the ESCAPE key.
+        while (!glfwWindowShouldClose(glfwWindow)) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
+            glfwSwapBuffers(glfwWindow); // swap the color buffers
+
+            // Poll for window events. The key callback above will only be
+            // invoked during this call.
+            glfwPollEvents();
+        }
+    }
+
+    private void freeWindowCallbacksAndDestroyWindow() {
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+    }
+}
