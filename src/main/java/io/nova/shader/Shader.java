feefa92671109655393d4792f3a5b37c52346019
Rami Nakkar <rami.nakkar@miragon.io>
2023-02-18 18:51:14 +0100

    refactor: changed Shader import to GL30

--- src/main/java/io/nova/shader/Shader.java
+++ src/main/java/io/nova/shader/Shader.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Objects;
 
-import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.*;
 
 public class Shader {
 
