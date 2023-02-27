db9a064cfc0671d20779d5c888a1a4fc6fe304bb
Rami Nakkar <rami.nakkar@miragon.io>
2023-02-27 19:43:51 +0100

    fix: remove GLFW mouseCodes prefix

--- src/main/java/io/nova/core/MouseCodes.java
+++ src/main/java/io/nova/core/MouseCodes.java
@@ -2,16 +2,16 @@ package io.nova.core;
 
 public class MouseCodes {
     public static final int
-            GLFW_MOUSE_BUTTON_1      = 0,
-            GLFW_MOUSE_BUTTON_2      = 1,
-            GLFW_MOUSE_BUTTON_3      = 2,
-            GLFW_MOUSE_BUTTON_4      = 3,
-            GLFW_MOUSE_BUTTON_5      = 4,
-            GLFW_MOUSE_BUTTON_6      = 5,
-            GLFW_MOUSE_BUTTON_7      = 6,
-            GLFW_MOUSE_BUTTON_8      = 7,
-            GLFW_MOUSE_BUTTON_LAST   = GLFW_MOUSE_BUTTON_8,
-            GLFW_MOUSE_BUTTON_LEFT   = GLFW_MOUSE_BUTTON_1,
-            GLFW_MOUSE_BUTTON_RIGHT  = GLFW_MOUSE_BUTTON_2,
-            GLFW_MOUSE_BUTTON_MIDDLE = GLFW_MOUSE_BUTTON_3;
+            NV_MOUSE_BUTTON_1      = 0,
+            NV_MOUSE_BUTTON_2      = 1,
+            NV_MOUSE_BUTTON_3      = 2,
+            NV_MOUSE_BUTTON_4      = 3,
+            NV_MOUSE_BUTTON_5      = 4,
+            NV_MOUSE_BUTTON_6      = 5,
+            NV_MOUSE_BUTTON_7      = 6,
+            NV_MOUSE_BUTTON_8      = 7,
+            NV_MOUSE_BUTTON_LAST   = NV_MOUSE_BUTTON_8,
+            NV_MOUSE_BUTTON_LEFT   = NV_MOUSE_BUTTON_1,
+            NV_MOUSE_BUTTON_RIGHT  = NV_MOUSE_BUTTON_2,
+            NV_MOUSE_BUTTON_MIDDLE = NV_MOUSE_BUTTON_3;
 }
