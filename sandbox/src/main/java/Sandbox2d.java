29b3c7b26ac1fa8670ed416afe7c91f64d3b7cad
Rami Nakkar <rami.nakkar@miragon.io>
2023-03-05 14:15:08 +0100

    feat: add rotated block

--- sandbox/src/main/java/Sandbox2d.java
+++ sandbox/src/main/java/Sandbox2d.java
@@ -6,10 +6,10 @@ import io.nova.core.renderer.camera.OrthographicCameraController;
 import io.nova.core.renderer.texture.TextureLibrary;
 import io.nova.event.Event;
 import org.joml.Vector2f;
-import org.joml.Vector4f;
 
 public class Sandbox2d extends Layer {
 
+    static float rotation = 0;
     private final float[] objectColor = new float[]{0.8f, 0.2f, 0.3f, 1.0f};
     private OrthographicCameraController cameraController;
     private Renderer renderer;
@@ -27,13 +27,11 @@ public class Sandbox2d extends Layer {
         renderer.setClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         renderer.clear();
 
+        rotation += deltaTime * 100;
         renderer.beginScene(cameraController.getCamera());
         {
-            renderer.drawQuad(new Vector2f(), new Vector2f(1.0f, 1.0f), new Vector4f(objectColor));
-            renderer.drawQuad(new Vector2f(0.5f, 0.5f), new Vector2f(1.0f, 1.0f), new Vector4f(0.5f));
-            renderer.drawQuad(new Vector2f(0.5f, 0.5f), new Vector2f(0.5f, 0.5f), TextureLibrary.getOrElseUploadTexture("Nova2d-logo-white.png"));
-            renderer.drawQuad(new Vector2f(-0.5f, -0.5f), new Vector2f(2.0f, 2.5f), TextureLibrary.getOrElseUploadTexture("openGlLogo.png"), 10.0f);
-            renderer.drawRotatedQuad(new Vector2f(-0.5f, -0.5f), new Vector2f(2.0f, 2.5f), 45.0f, TextureLibrary.getOrElseUploadTexture("openGlLogo.png"), 10.0f, new Vector4f(0.2f, 0.1f, 0.8f, 0.5f));
+            renderer.drawRotatedQuad(new Vector2f(), new Vector2f(0.16f, 0.16f), rotation, TextureLibrary.getOrElseUploadTexture("beton-block.png"));
+            renderer.drawRotatedQuad(new Vector2f(-0.16f * 2, 0), new Vector2f(0.16f, 0.16f), -rotation, TextureLibrary.getOrElseUploadTexture("beton-block.png"));
         }
         renderer.endScene();
     }
