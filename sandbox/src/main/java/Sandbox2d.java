d8ff02f7852852aee501a9aa4f7613da79b1ae21
Rami Nakkar <rami.nakkar@miragon.io>
2023-03-07 19:37:51 +0100

    fix: removed dockspace stuff out of sandbox

--- sandbox/src/main/java/Sandbox2d.java
+++ sandbox/src/main/java/Sandbox2d.java
@@ -1,10 +1,7 @@
 import imgui.ImGui;
-import imgui.flag.ImGuiCond;
-import imgui.flag.ImGuiStyleVar;
-import imgui.flag.ImGuiWindowFlags;
-import io.nova.core.application.Application;
 import io.nova.core.layer.Layer;
-import io.nova.core.renderer.*;
+import io.nova.core.renderer.Renderer;
+import io.nova.core.renderer.RendererFactory;
 import io.nova.core.renderer.camera.OrthographicCameraController;
 import io.nova.core.renderer.texture.SubTexture;
 import io.nova.core.renderer.texture.Texture;
@@ -23,7 +20,6 @@ public class Sandbox2d extends Layer {
     private SubTexture grass;
     private SubTexture stone;
     private SubTexture ceiling;
-    private FrameBuffer frameBuffer;
 
     @Override
     public void onAttach() {
@@ -33,16 +29,12 @@ public class Sandbox2d extends Layer {
         grass = new SubTexture(texture, new Vector2f(0, 10), new Vector2f(16.0f, 16.0f));
         stone = new SubTexture(texture, new Vector2f(1, 1), new Vector2f(16.0f, 16.0f));
         ceiling = new SubTexture(texture, new Vector2f(6, 6), new Vector2f(16.0f, 16.0f));
-
-        var spec = new FrameBufferSpecification(Application.getWindow().getWidth(), Application.getWindow().getHeight());
-        frameBuffer = FrameBufferFactory.create(spec);
     }
 
     @Override
     public void onUpdate(float deltaTime) {
         cameraController.onUpdate(deltaTime);
 
-        frameBuffer.bind();
         renderer.setClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         renderer.clear();
 
@@ -54,61 +46,18 @@ public class Sandbox2d extends Layer {
             renderer.drawRotatedQuad(new Vector2f(2.0f, 0.0f), new Vector2f(1.0f, 1.0f), (float) Math.toRadians(45.0f), ceiling);
         }
         renderer.endScene();
-        frameBuffer.unbind();
     }
 
     @Override
     public void onImGuiRender() {
-//        ImGui.colorEdit4("Object color", objectColor);
-//
-//        var stats = renderer.getStats();
-//        ImGui.text("Renderer stats:");
-//        ImGui.text("Draw calls: " + stats.getDrawCalls());
-//        ImGui.text("Quad count: " + stats.getQuadCount());
-//        ImGui.text("Vertex count: " + stats.getTotalVertexCount());
-//        ImGui.text("Index count: " + stats.getTotalIndexCount());
+        ImGui.colorEdit4("Object color", objectColor);
 
-        // We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
-        // because it would be confusing to have two docking targets within each others.
-        var window_flags = ImGuiWindowFlags.MenuBar | ImGuiWindowFlags.NoDocking;
-        var viewport = ImGui.getMainViewport();
-
-        ImGui.setNextWindowPos(viewport.getPosX(), viewport.getPosY(), ImGuiCond.Always);
-        ImGui.setNextWindowSize(viewport.getWorkSizeX(), viewport.getWorkSizeY());
-        ImGui.setNextWindowViewport(viewport.getID());
-
-        ImGui.pushStyleVar(ImGuiStyleVar.WindowRounding, 0.0f);
-        ImGui.pushStyleVar(ImGuiStyleVar.WindowBorderSize, 0.0f);
-
-        window_flags |= ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoMove;
-        window_flags |= ImGuiWindowFlags.NoBringToFrontOnFocus | ImGuiWindowFlags.NoNavFocus;
-
-        ImGui.begin("Dockspace", window_flags);
-
-        ImGui.popStyleVar(2);
-
-        ImGui.dockSpace(ImGui.getID("Dockspace"));
-
-        ImGui.beginMenuBar();
-        if (ImGui.beginMenu("File")) {
-            if (ImGui.menuItem("Exit")) {
-                Application.getInstance().close();
-            }
-            ImGui.endMenu();
-        }
-        ImGui.endMenuBar();
-
-        ImGui.begin("Renderer stats");
         var stats = renderer.getStats();
+        ImGui.text("Renderer stats:");
         ImGui.text("Draw calls: " + stats.getDrawCalls());
         ImGui.text("Quad count: " + stats.getQuadCount());
         ImGui.text("Vertex count: " + stats.getTotalVertexCount());
         ImGui.text("Index count: " + stats.getTotalIndexCount());
-
-        var textureId = frameBuffer.getColorAttachmentRendererId();
-        ImGui.image(textureId, 1000, 800);
-        ImGui.end();
-        ImGui.end();
     }
 
     @Override
