68b6aea906d534635ba5de89f72eb85fc0c720b8
Rami Nakkar <rami.nakkar@miragon.io>
2023-03-12 17:54:01 +0100

    feat: add properties panel for tag component and ablity to change it

--- SuperNova/src/main/java/EntityPanel.java
+++ SuperNova/src/main/java/EntityPanel.java
@@ -1,8 +1,11 @@
 import imgui.ImGui;
 import imgui.flag.ImGuiTreeNodeFlags;
+import imgui.type.ImString;
 import io.nova.ecs.Scene;
 import io.nova.ecs.component.TagComponent;
 import io.nova.ecs.entity.Entity;
+
+import java.util.Objects;
 
 public class EntityPanel {
 
@@ -17,6 +20,12 @@ public class EntityPanel {
         ImGui.begin("Entities");
         for (var entity : scene.getRegistry().getEntities()) {
             drawEntityNode(entity);
+        }
+        ImGui.end();
+
+        ImGui.begin("Properties");
+        if (Objects.nonNull(selectedEntity)) {
+            drawComponents(selectedEntity);
         }
         ImGui.end();
     }
@@ -34,6 +43,16 @@ public class EntityPanel {
 
         if (expanded) {
             ImGui.treePop();
+        }
+    }
+
+    private void drawComponents(Entity entity) {
+        if (entity.hasComponent(TagComponent.class)) {
+            var tag = entity.getComponent(TagComponent.class);
+            var string = new ImString(tag.getTag(), ImString.DEFAULT_LENGTH);
+            if (ImGui.inputText("Tag", string)) {
+                tag.setTag(string.get());
+            }
         }
     }
 }
--- src/main/java/io/nova/ecs/component/TagComponent.java
+++ src/main/java/io/nova/ecs/component/TagComponent.java
@@ -1,30 +1,25 @@
 package io.nova.ecs.component;
 
-import java.util.UUID;
-
 public class TagComponent extends Component {
-    private final String tag;
+    private String tag;
 
     public TagComponent() {
-        this(generateId());
+        this("");
     }
 
     public TagComponent(String tag) {
-        this.tag = isInvalid(tag) ? generateId() : tag;
+        this.tag = isInvalid(tag) ? "Unnamed Entity" : tag;
     }
 
     private static boolean isInvalid(String tag) {
         return tag == null || tag.isEmpty() || tag.isBlank();
     }
 
-    private static String generateId() {
-        var hex = UUID.randomUUID().toString()
-                .replace("-", "")
-                .substring(0, 8);
-        return "Entity@" + hex;
-    }
-
     public String getTag() {
         return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
     }
 }
