b99d21ab87ae29cb026b1837e0c348f03638c933
Rami Nakkar <rami.nakkar@miragon.io>
2023-03-15 15:55:34 +0100

    feat: add button ui to remove component

--- SuperNova/src/main/java/panels/properties/ComponentNode.java
+++ SuperNova/src/main/java/panels/properties/ComponentNode.java
@@ -1,6 +1,7 @@
 package panels.properties;
 
 import imgui.ImGui;
+import imgui.flag.ImGuiStyleVar;
 import imgui.flag.ImGuiTreeNodeFlags;
 import io.nova.ecs.component.Component;
 import io.nova.ecs.entity.Entity;
@@ -9,9 +10,34 @@ public class ComponentNode {
 
     public static <T extends Component> void create(Entity entity, String name, Class<T> clazz, Runnable content) {
         if (entity.hasComponent(clazz)) {
-            if (ImGui.treeNodeEx(name, ImGuiTreeNodeFlags.DefaultOpen | ImGuiTreeNodeFlags.SpanAvailWidth)) {
+            var contentRegionAvail = ImGui.getContentRegionAvail();
+            ImGui.pushStyleVar(ImGuiStyleVar.FramePadding, 4, 4);
+            float lineHeight = ImGui.getFontSize() + ImGui.getStyle().getFramePadding().y * 2.0f;
+
+            ImGui.separator();
+            var open = ImGui.treeNodeEx(name, ImGuiTreeNodeFlags.DefaultOpen | ImGuiTreeNodeFlags.Framed | ImGuiTreeNodeFlags.FramePadding | ImGuiTreeNodeFlags.AllowItemOverlap);
+            ImGui.popStyleVar();
+
+            ImGui.sameLine(contentRegionAvail.x - lineHeight * 0.5f);
+            if (ImGui.button("+", lineHeight, lineHeight)) {
+                ImGui.openPopup("ComponentSettings");
+            }
+
+            boolean removeComponent = false;
+            if (ImGui.beginPopup("ComponentSettings")) {
+                if (ImGui.menuItem("Remove component")) {
+                    removeComponent = true;
+                }
+                ImGui.endPopup();
+            }
+
+            if (open) {
                 content.run();
                 ImGui.treePop();
+            }
+
+            if (removeComponent) {
+                // TODO: remove component
             }
         }
     }
