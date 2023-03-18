304f98d771929913065be86bc5c460c4f25ebf69
Rami Nakkar <rami.nakkar@miragon.io>
2023-03-18 13:50:55 +0100

    feat: init content browsing

--- SuperNova/src/main/java/panels/ContentBrowserPanel.java
+++ SuperNova/src/main/java/panels/ContentBrowserPanel.java
@@ -3,22 +3,43 @@ package panels;
 import imgui.ImGui;
 
 import java.io.File;
+import java.nio.file.Path;
 import java.util.Objects;
 
 public class ContentBrowserPanel {
 
+    private File currentDirectory;
+
+    public ContentBrowserPanel() {
+        var userDir = System.getProperty("user.dir");
+        var path = Path.of(userDir, "SuperNova", "src", "main", "resources");
+        currentDirectory = new File(path.toUri());
+    }
+
     public void onImGuiRender() {
         ImGui.begin("Content browser");
 
-        var currentDirectory = new File(System.getProperty("user.dir"));
-        var files = currentDirectory.listFiles();
-
-        if (Objects.nonNull(files)) {
-            for (var file : files) {
-                ImGui.text(file.getName());
-            }
-        }
+        createDirectoryContent(currentDirectory);
 
         ImGui.end();
+    }
+
+    private void createDirectoryContent(File currentFile) {
+        if (ImGui.button("<-")) {
+            currentDirectory = currentFile.getParentFile();
+        }
+
+        var files = currentFile.listFiles();
+        if (Objects.nonNull(files)) {
+            for (var file : files) {
+                if (file.isDirectory()) {
+                    if (ImGui.button(file.getName() + "##" + file.getName())) {
+                        currentDirectory = file;
+                    }
+                } else {
+                    ImGui.text(file.getName());
+                }
+            }
+        }
     }
 }
